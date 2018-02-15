cb9120d25580e30ee5cab92ea0ffd5544bb2f67c
lukpecyn <lukpecyn@poczta.onet.pl>
2018-02-15 20:33:30 +0100

    0.3.0.9
    Rodzielenie funkcji dodawania i modyfikacji rozliczeń (payment).

--- miniGRANT/src/main/java/pl/lukpecyn/minigrant/controllers/PaymentController.java
+++ miniGRANT/src/main/java/pl/lukpecyn/minigrant/controllers/PaymentController.java
@@ -63,33 +63,20 @@ public class PaymentController {
 	@Autowired
 	PaymentService paymentService;
 	
-	@GetMapping({"/grant/{idGrant}/document/{idDocument}/payment_form","/grant/{idGrant}/document/{idDocument}/payment/{idPayment}/payment_form"})
-	public String paymentFormGet(Payment payment,
-			Document document,
-			Model model, 
+	@GetMapping("/grant/{idGrant}/document/{idDocument}/payment_form")
+	public String addPaymentFormGet(Model model,
 			@PathVariable(value="idGrant", required=true) Integer idGrant, 
-			@PathVariable(value="idDocument", required=true) Integer idDocument,
-			@PathVariable(value="idPayment", required=false) Integer idPayment) {
+			@PathVariable(value="idDocument", required=true) Integer idDocument) {
 		model.addAttribute("appVersion", appVersion);
 		model.addAttribute("appName", appName);
 				
-		if(idPayment!=null) {
-			payment = paymentService.getPayment(idPayment);
-			if(payment.getId()!=null) {
-				model.addAttribute("payment", payment);
+		Document document = documentService.getDocument(idDocument);
+		model.addAttribute("document", document);
 			
-				document = documentService.getDocument(payment.getDocument().getId());
-				model.addAttribute("document", document);
-			} 
-		} else {
-			document = documentService.getDocument(idDocument);
-			model.addAttribute("document", document);
-			
-			payment.setDocument(document);
-			model.addAttribute("payment", payment);
-		}
-
-		
+		Payment payment = new Payment();
+		payment.setDocument(document);
+		model.addAttribute("payment", payment);
+				
 		Grant grant = grantService.getGrant(document.getIdGrant());
 		model.addAttribute("grant", grant);
 
@@ -99,12 +86,36 @@ public class PaymentController {
 		return "payment_form";
 	}
 
-	@PostMapping({"/grant/{idGrant}/document/{idDocument}/payment_form","/grant/{idGrant}/document/{idDocument}/payment/{idPayment}/payment_form"})
-	public String paymentFormPost(Payment payment,
-			Model model, 
+	@GetMapping("/grant/{idGrant}/document/{idDocument}/payment/{idPayment}/payment_form")
+	public String updatePaymentFormGet(Model model, Payment payment, Document document,
 			@PathVariable(value="idGrant", required=true) Integer idGrant, 
 			@PathVariable(value="idDocument", required=true) Integer idDocument,
-			@PathVariable(value="idPayment", required=false) Integer idPayment) {
+			@PathVariable(value="idPayment", required=true) Integer idPayment) {
+		model.addAttribute("appVersion", appVersion);
+		model.addAttribute("appName", appName);
+				
+		if(idPayment!=null) {
+			payment = paymentService.getPayment(idPayment);
+			model.addAttribute("payment", payment);
+			
+			document = documentService.getDocument(payment.getDocument().getId());
+			model.addAttribute("document", document);
+			
+			Grant grant = grantService.getGrant(document.getIdGrant());
+			model.addAttribute("grant", grant);
+
+			List<Budget> budgetList = budgetService.getBudgetForGrantList(grant.getId());
+			model.addAttribute("budgetList", budgetList);
+
+			return "payment_form";
+		} 
+		return "redirect:/grant";
+	}
+
+	@PostMapping("/grant/{idGrant}/document/{idDocument}/payment_form")
+	public String addPaymentFormPost(Model model, Payment payment,
+			@PathVariable(value="idGrant", required=true) Integer idGrant, 
+			@PathVariable(value="idDocument", required=true) Integer idDocument) {
 		model.addAttribute("appVersion", appVersion);
 		model.addAttribute("appName", appName);
 
@@ -122,12 +133,65 @@ public class PaymentController {
 							.add(paymentService.getPaymentForDocumentContributionOwnSum(document.getId())
 									.add(paymentService.getPaymentForDocumentContributionPersonalSum(document.getId())
 											.add(paymentService.getPaymentForDocumentContributionInkindSum(document.getId())))));
-			if(idPayment!=null) {
-				payment.setBudget(budget);
-				unpaidDocument.add(payment.getSum());
-			}
-			model.addAttribute("payment", payment);
 
+			if((unpaidDotation.subtract(payment.getDotation()).signum()==-1) ||
+					(unpaidContributionOwn.subtract(payment.getContributionOwn()).signum()==-1) ||
+					(unpaidContributionPersonal.subtract(payment.getContributionPersonal()).signum()==-1) ||
+					(unpaidContributionInkind.subtract(payment.getContributionInkind()).signum()==-1) ||
+					(unpaidDocument.subtract(payment.getDotation().add(payment.getContributionOwn().add(payment.getContributionPersonal().add(payment.getContributionInkind())))).signum()==-1)) {
+				if(unpaidDotation.subtract(payment.getDotation()).signum()==-1) 
+					model.addAttribute("unpaidDotation", unpaidDotation);
+				if(unpaidContributionOwn.subtract(payment.getContributionOwn()).signum()==-1) 
+					model.addAttribute("unpaidContributionOwn", unpaidContributionOwn);
+				if(unpaidContributionPersonal.subtract(payment.getContributionPersonal()).signum()==-1)
+					model.addAttribute("unpaidContributionPersonal", unpaidContributionPersonal);
+				if(unpaidContributionInkind.subtract(payment.getContributionInkind()).signum()==-1)
+					model.addAttribute("unpaidContributionInkind", unpaidContributionInkind);
+				if(unpaidDocument.subtract(payment.getDotation().add(payment.getContributionOwn().add(payment.getContributionPersonal().add(payment.getContributionInkind())))).signum()==-1)
+					model.addAttribute("unpaidDocument", unpaidDocument);
+			
+				model.addAttribute("payment", payment);
+				model.addAttribute("document", document);
+				model.addAttribute("grant", grant);
+				List<Budget> budgetList = budgetService.getBudgetForGrantList(grant.getId());
+				model.addAttribute("budgetList", budgetList);
+				return "payment_form";
+			} else {
+				paymentService.addPayment(payment);
+			}
+			return "redirect:/grant/" + grant.getId() + "/document/" + document.getId();
+		}
+		return "redirect:/";
+	}
+
+	@PostMapping("/grant/{idGrant}/document/{idDocument}/payment/{idPayment}/payment_form")
+	public String updatePaymentFormPost(Model model, Payment payment,
+			@PathVariable(value="idGrant", required=true) Integer idGrant, 
+			@PathVariable(value="idDocument", required=true) Integer idDocument,
+			@PathVariable(value="idPayment", required=true) Integer idPayment) {
+		model.addAttribute("appVersion", appVersion);
+		model.addAttribute("appName", appName);
+
+		if(payment.getId()==idPayment) {
+			Document document = documentService.getDocument(payment.getDocument().getId());
+			Grant grant = grantService.getGrant(document.getIdGrant());
+			Budget budget = budgetService.getBudget(payment.getBudget().getId());
+
+			BigDecimal unpaidDotation = budget.getDotation().subtract(budget.getPaidDotation());
+			BigDecimal unpaidContributionOwn = budget.getContributionOwn().subtract(budget.getPaidContributionOwn());
+			BigDecimal unpaidContributionPersonal = budget.getContributionPersonal().subtract(budget.getPaidContributionPersonal());
+			BigDecimal unpaidContributionInkind = budget.getContributionInkind().subtract(budget.getPaidContributionInkind());
+			BigDecimal unpaidDocument = document.getValue()
+					.subtract(paymentService.getPaymentForDocumentDotationSum(document.getId())
+							.add(paymentService.getPaymentForDocumentContributionOwnSum(document.getId())
+									.add(paymentService.getPaymentForDocumentContributionPersonalSum(document.getId())
+											.add(paymentService.getPaymentForDocumentContributionInkindSum(document.getId())))));
+			
+			payment.setBudget(budget);
+			model.addAttribute("payment", payment);
+			
+			unpaidDocument.add(payment.getSum());
+			
 			if((unpaidDotation.subtract(payment.getDotation()).signum()==-1) ||
 					(unpaidContributionOwn.subtract(payment.getContributionOwn()).signum()==-1) ||
 					(unpaidContributionPersonal.subtract(payment.getContributionPersonal()).signum()==-1) ||
@@ -150,13 +214,9 @@ public class PaymentController {
 				model.addAttribute("budgetList", budgetList);
 				return "payment_form";
 			} else {
-				if(payment.getId()>=0) {
-					paymentService.updatePayment(payment);
-				} else {
-					paymentService.addPayment(payment);
-				}
+				paymentService.updatePayment(payment);
 			}
-		return "redirect:/grant/" + grant.getId() + "/document/" + document.getId();
+			return "redirect:/grant/" + grant.getId() + "/document/" + document.getId();
 		}
 		return "redirect:/";
 	}
